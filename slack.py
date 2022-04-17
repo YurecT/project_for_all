bbc228c539d39262b92e9cb97aea01e982445260
YurecT <Yurecschors@gmail.com>
2022-04-17 17:39:45 +0300

    slack

---
+++ slack.py
@@ -0,0 +1,32 @@
+from slack_sdk.webhook import WebhookClient
+url = "https://hooks.slack.com/services/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"
+webhook = WebhookClient(url)
+
+response = webhook.send(
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "_______________________________________________________"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": " *Login:*\n passed: 10, failed: 1"
+            }
+        },
+{
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "A very important thing has occurred! <https://alert-system.com/alerts/1234|Click here> for details!"
+            }
+        }
+    ]
+)
+
+assert response.status_code == 200
+assert response.body == "ok"
